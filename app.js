5dc2e5adc3ff5a1eded978f1c9c25ac83163babf
Fauzaro01 <MuhamadFauzan4750@gmail.com>
2022-05-26 14:25:12 +0700

    simple

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var expressLayout = require('express-ejs-layouts');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayout);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    layout: 'layouts/main'
+  });
+});
+
+router.get('/project-list', function(req, res, next) {
+  res.render('project', {
+    layout: 'layouts/main'
+  });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+    layout: 'layouts/main'
+  });
 });
 
 module.exports = router;
