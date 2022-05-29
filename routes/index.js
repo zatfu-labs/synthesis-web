aa2e4511cf2b387c092a324cab7ce21a489c3b0e
Fauzaro01 <MuhamadFauzan4750@gmail.com>
2022-05-29 22:06:06 +0700

    update again :/

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    layout: 'layouts/main'
+    layout: 'layouts/main',
+    title: "Home"
   });
 });
 
 router.get('/project-list', function(req, res, next) {
   res.render('project', {
-    layout: 'layouts/main'
+    layout: 'layouts/main',
+    title: "Project List"
   });
 });
 
 router.get('/about', function(req, res, next) {
   res.render('about', {
-    layout: 'layouts/main'
+    layout: 'layouts/main',
+    title: "About Team"
   });
 });
 
