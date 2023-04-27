92cb01535819ff794dd7403d2fb02cae13d1e56f
Fauzaro01 <MuhamadFauzan4750@gmail.com>
2023-04-27 16:19:15 +0700

    Add Route GET /ceklimit

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { cekKey } = require('../database/db');
+const { cekKey, cekLimit } = require('../database/db');
 const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
 const { cakLontong, bijak, quotes, fakta, motivasi, quotenime, husbu, loli } = require('../controllers/randomtext');
 const Scraper = require('../controllers/scraper/main');
@@ -23,6 +23,26 @@ router.get('/checkkey', async (req, res) => {
 	}
 
 	res.send({ status: 200, apikey, response: 'Active' });
+});
+
+router.post('/ceklimit', async (req, res) => {
+	const { apikey } = req.query;
+	if (apikey === undefined) {
+		return res.status(404).send({
+			status: 404,
+			message: 'Input Parameter apikey',
+		});
+	}
+
+	const check = await cekLimit(apikey);
+	if (!check) {
+		return res.status(403).send({
+			status: 403,
+			message: `apikey ${apikey} not found!`,
+		});
+	}
+
+	res.send({ status: 200, apikey, limit: check });
 });
 
 // LIST A API V1 From Scrapper To Function
