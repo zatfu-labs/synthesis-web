c2b70c1ff933de57fda83e8f54e8b1c34f99a264
Muhamad Fauzan <MuhamadFauzan4750@gmail.com>
2023-05-18 10:43:37 +0700

    Menonaktifkan Sementara API Agar Tidak Lag

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { cekKey, cekLimit } = require('../database/db');
 const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
 const { cakLontong, bijak, quotes, fakta, motivasi, quotenime, husbu, loli } = require('../controllers/randomtext');
-const Scraper = require('../controllers/scraper/main');
 
 router.get('/checkkey', async (req, res) => {
 	const { apikey } = req.query;
@@ -49,14 +48,6 @@ router.post('/ceklimit', async (req, res) => {
 router.get('/ytplay', youtubePlay);
 router.get('/ytmp4', youtubeMp4);
 router.get('/ytmp3', youtubeMp3);
-router.get('/ghinfo', Scraper.Github.userInfo);
-router.get('/weather', Scraper.Weather.weather);
-router.get('/iplookup', Scraper.Iplookup.iplookup);
-router.get('/hilih', Scraper.Word.Hilih);
-router.get('/halah', Scraper.Word.Halah);
-router.get('/gempa', Scraper.Bmkg.Gempa);
-router.get('/pinterest', Scraper.Pinterest.Search);
-router.get('/shorturl', Scraper.TinyURL.short);
 router.get('/caklontong', cakLontong);
 router.get('/quotenime', quotenime);
 router.get('/quotes', quotes);
@@ -65,12 +56,6 @@ router.get('/bijak', bijak);
 router.get('/motivasi', motivasi);
 router.get('/loli', loli);
 router.get('/husbu', husbu);
-
-/*
-------------------------------------------------------------------------------------------
-------------------------------[API V2 Experiment]-----------------------------------------
-------------------------------------------------------------------------------------------
-*/
 
 /*
 ------------------------------------------------------------------------------------------
